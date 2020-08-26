4a7bfe3ecdfe973801482eceb66e2f5a4d40e973
Nufreort <ryryry203@gmail.com>
2020-08-26 16:40:10 +0200

    all main issues solved.

---
+++ migrations/Version20200826092323.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200826092323 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE task CHANGE created_by_id created_by_id INT DEFAULT NULL, CHANGE start_at start_at DATETIME DEFAULT NULL, CHANGE end_at end_at DATETIME DEFAULT NULL, CHANGE updated_at updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL, CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE updated_at updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE task CHANGE created_by_id created_by_id INT DEFAULT NULL, CHANGE start_at start_at DATETIME DEFAULT \'NULL\', CHANGE end_at end_at DATETIME DEFAULT \'NULL\', CHANGE updated_at updated_at DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`, CHANGE created_at created_at DATETIME DEFAULT \'current_timestamp()\' NOT NULL, CHANGE updated_at updated_at DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
+    }
+}
--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -5,12 +5,41 @@ namespace Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use App\Entity\User;
 
 class TaskControllerTest extends WebTestCase
 {
-    public function testHomePage() {
-        $client = static::createClient();
-        $crawlerr = $client->request('GET', '/');
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    public function testTestAreWorking()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+
+    public function testCreateTask()
+    {
+        $user = $this->getMockBuilder('DB\User');
+        $user->disableOriginalConstructor();
+        $user->getMock();
+
+        $title = 'Ma tâche';
+        $content = 'Ma description de tâche';
+
+        $task = new Task($title, $content);
+        $task->setCreatedAt(new \DateTime());
+        $task->setTitle($title);
+        $task->setContent($content);
+        $task->setCreatedBy($user);
+
+        return $task;
+    }
+
+    public function assertHasErrors(Task $task, int $number = 0) {
+        self::bootKernel();
+        $error = self::$container->get('validator')->validate($task);
+        $this->assertCount($number, $error);
+    }
+
+    public function testValidEntity() {
+        $this->assertHasErrors($this->testCreateTask(), 0);
     }
 }
