4a8887051b63addab110f46ca0f24b66709d2edc
Nufreort <ryryry203@gmail.com>
2020-09-02 21:39:51 +0200

    test

--- README.md
+++ README.md
@@ -4,3 +4,36 @@ ToDoList
 Base du projet #8 : Améliorez un projet existant
 
 https://openclassrooms.com/projects/ameliorer-un-projet-existant-1
+
+## Getting Started
+
+### Prerequisites
+
+To run this project in local you need a local web server as a wampserver for Windows (Apache, PHP, MySQL).
+
+### Installing
+
+1 - You need to install Symfony on your computer. To do it you can use composer.
+
+2 - You should create a database and import the initial set of data in it.
+
+3 - Clone the repository on your computer.
+
+4 - Use terminal to initialise a new project from local repository and database.
+
+### Coding style tests and analysis
+
+Tests done with :
+    Codacy - (<https://www.codacy.com>)
+
+## Built With
+
+    [Symfony - v5.1] - (<https://symfony.com/>) - PHP framework
+
+## Authors
+
+    Made by - Nufreort
+
+## Acknowledgments
+
+    Mentor - Dadouda
--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -7,9 +7,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Task;
 use App\Entity\User;
+use Symfony\Component\DomCrawler\Crawler;
+use App\Tests\Controller\SecurityControllerTest;
 
 class TaskControllerTest extends WebTestCase
 {
+    public function getTestUser() {
+      $username = 'TestUser';
+      $password = 'password';
+      $email = 'contact@mailtest.com';
+      $roles = ['ROLE_USER'];
+
+      $user = new User($username, $password, $email, $roles);
+      $user->setUsername($username);
+      $user->setPassword($password);
+      $user->setEmail($email);
+      $user->setRoles($roles);
+
+      return $user;
+    }
+
     public function testTestAreWorking()
     {
         $this->assertEquals(2, 1 + 1);
@@ -17,9 +34,16 @@ class TaskControllerTest extends WebTestCase
 
     public function testCreateTask()
     {
-        $user = $this->getMockBuilder('DB\User');
-        $user->disableOriginalConstructor();
-        $user->getMock();
+      $username = 'TestUser';
+      $password = 'password';
+      $email = 'contact@mailtest.com';
+      $roles = ['ROLE_USER'];
+
+      $user = new User($username, $password, $email, $roles);
+      $user->setUsername($username);
+      $user->setPassword($password);
+      $user->setEmail($email);
+      $user->setRoles($roles);
 
         $title = 'Ma tâche';
         $content = 'Ma description de tâche';
@@ -42,4 +66,62 @@ class TaskControllerTest extends WebTestCase
     public function testValidEntity() {
         $this->assertHasErrors($this->testCreateTask(), 0);
     }
+
+    //-------------- ACTIONS
+    public function testListAction()
+    {
+        $securityControllerTest = new SecurityControllerTest();
+        $client = $securityControllerTest->testLogin();
+
+        $crawler = $client->request('GET', '/tasks');
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+
+    //------------PAGES STATUS AND CONTENT ---------------
+    public function testHomePageIsUp() {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testTaskListPage() {
+        $client = static::createClient();
+        $client->request('GET', '/tasks');
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testTaskListContent() {
+        $client = static::createClient();
+        $client->request('GET', '/tasks');
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testTaskCreationPage() {
+        $client = static::createClient();
+        $client->request('GET', 'tasks/create');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testTaskEditionPage() {
+        $client = static::createClient();
+        $client->request('GET', '/tasks/{id}/edit');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testTaskDoneAction() {
+        $client = static::createClient();
+        $client->request('GET', '/tasks/{id}/toggle');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testTaskDeletingAction() {
+        $client = static::createClient();
+        $client->request('GET', '/tasks/{id}/delete');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
 }
